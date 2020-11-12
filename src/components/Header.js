970e64e018f4f19d999af3475505180b0378216b
ecru <woovoodoor@yahoo.co.jp>
2020-11-12 17:27:51 +0900

    ヘッダーの修正

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,11 +6,11 @@ class Header extends React.Component {
   render() {
     return(
       <div className= "header__link__list">
-        <h2> TOMOYUKI KUMAGAI </h2>
+        <h4> TOMOYUKI KUMAGAI </h4>
         <div className= "header__list">
           <li className="header__list"><Link className="header__link" to="/">HOME</Link></li>
           <li className="header__list"><Link className="header__link" to="/about">ABOUT</Link></li>
-          <li className="header__list"><Link className="header__link" to="/work">WORK</Link></li>
+          <li className="header__list"><Link className="header__link" to="/works">WORK</Link></li>
           <li className="header__list"><Link className="header__link" to="/contact">CONTACT</Link></li>
         </div>
       </div>
