fc9d60320559719ebf33731bd52347037d8b2062
ecru <woovoodoor@yahoo.co.jp>
2020-12-11 15:50:53 +0900

    修正

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'; //fontawesomeのコ
 import { fab } from '@fortawesome/free-brands-svg-icons'; //fontawesomeのbrandアイコンのインポート
 import { fas } from '@fortawesome/free-solid-svg-icons'; //fontawesomeのsolidアイコンのインポート
 import { far } from '@fortawesome/free-regular-svg-icons'; //fontawesomeのregularアイコンのインポート
-library.add(fab, fas, far); //他のコンポーネントから簡単に呼び出せるようにするための登録処理？
+library.add(fab, fas, far);
 
 class App extends React.Component {
   render() {
